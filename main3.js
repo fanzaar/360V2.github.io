4eb2a22c35eb4b13f519641cc46af1ed71cc3225
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-06-19 21:45:04 +0530

    Update main3.js

--- main3.js
+++ main3.js
@@ -1,4 +1,4 @@
-const panoramaImage = new PANOLENS.ImagePanorama("");
+const panoramaImage = new PANOLENS.ImagePanorama("https://github.com/fanzaar/360V2.github.io/blob/main/images/image3.jpg");
 const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({

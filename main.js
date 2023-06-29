4473f6951c03d92a0e1ee16e781acfdfc8600e37
fanzaar <fanzar.25@gmail.com>
2023-06-30 02:40:13 +0530

    nav3.0

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
 const imageContainer = document.querySelector(".image-container");
 
@@ -9,3 +10,50 @@ const viewer = new PANOLENS.Viewer({
 });
 
 viewer.add(panoramaImage);
+
+// Example values for pitch and yaw angle ranges
+const minPitch = -Math.PI / 6; // Example minimum pitch angle (-30 degrees)
+const maxPitch = Math.PI / 6; // Example maximum pitch angle (30 degrees)
+const minYaw = -Math.PI / 3; // Example minimum yaw angle (-60 degrees)
+const maxYaw = Math.PI / 3; // Example maximum yaw angle (60 degrees)
+
+// Function to check if the specific location is in view
+function isLocationInView() {
+  const camera = viewer.getCamera();
+  const currentPitch = camera.pitch; // Get the current camera pitch angle
+  const currentYaw = camera.yaw; // Get the current camera yaw angle
+
+  // Check if the current pitch and yaw angles are within the desired ranges
+  if (
+    currentPitch >= minPitch &&
+    currentPitch <= maxPitch &&
+    currentYaw >= minYaw &&
+    currentYaw <= maxYaw
+  ) {
+    return true; // The specific location is in view
+  } else {
+    return false; // The specific location is not in view
+  }
+}
+
+// Create and display an arrow button if the location is in view
+function createArrowButton() {
+  if (isLocationInView()) {
+    const arrowButton = document.createElement("div");
+    arrowButton.classList.add("arrow-button");
+    // Set the arrow button's position and style as needed
+    // For example:
+    arrowButton.style.position = "fixed";
+    arrowButton.style.top = "50px";
+    arrowButton.style.left = "50px";
+    arrowButton.style.backgroundImage = "url('arrow.png')";
+    arrowButton.style.width = "40px";
+    arrowButton.style.height = "40px";
+    // Add event listeners or functionality to the arrow button if needed
+
+    document.body.appendChild(arrowButton);
+  }
+}
+
+// Example usage of createArrowButton()
+createArrowButton();

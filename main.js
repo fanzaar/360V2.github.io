94203d5385ac07ea26a43c6190633886788c3294
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-07-05 11:41:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,9 +10,13 @@ const viewer = new PANOLENS.Viewer({ container: imageContainer });
 // Add the panorama image to the viewer
 viewer.add(panoramaImage);
 
+// Create a separate div for displaying the position
+const positionDisplay = document.createElement("div");
+positionDisplay.setAttribute("id", "position-display");
+document.body.appendChild(positionDisplay);
+
 // Event listener for mouse click
 viewer.container.addEventListener("click", (event) => {
   const position = viewer.getControlPosition(event);
-  console.log("Position:", position);
+  positionDisplay.textContent = `Position: x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(2)}, z: ${position.z.toFixed(2)}`;
 });
-

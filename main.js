b3b729eebd256c17d617120fb059c8e428209d56
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-07-06 22:25:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,17 +10,8 @@ const viewer = new PANOLENS.Viewer({ container: imageContainer });
 // Add the panorama image to the viewer
 viewer.add(panoramaImage);
 
-// Manually specify the coordinates for the infospot
-const infospotX = -4797.28; // X-coordinate of infospot
-const infospotY = -965.44; // Y-coordinate of infospot
-const infospotZ = -1003.95; // Z-coordinate of infospot
-
-// Create an infospot at the specified position
-const infospot = new PANOLENS.Infospot(300, "images/arrow.png");
-infospot.position.set(infospotX, infospotY, infospotZ);
-viewer.add(infospot);
-
-// Event listener for infospot click
-infospot.addEventListener("click", () => {
-  window.location.href = "https://youtube.com"; // Replace with your desired URL
+// Event listener for mouse click
+viewer.container.addEventListener("click", (event) => {
+  const position = viewer.getControlPosition(event);
+  console.log("Position:", position);
 });

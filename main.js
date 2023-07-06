0e6ae99af0580ab578d00d7008f7909d8aa8c0ef
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-07-06 22:01:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,30 +10,17 @@ const viewer = new PANOLENS.Viewer({ container: imageContainer });
 // Add the panorama image to the viewer
 viewer.add(panoramaImage);
 
-// Create a separate div for displaying the position
-const positionDisplay = document.createElement("div");
-positionDisplay.setAttribute("id", "position-display");
-document.body.appendChild(positionDisplay);
+// Manually specify the coordinates for the infospot
+const infospotX = -4797.28; // X-coordinate of infospot
+const infospotY = -965.44; // Y-coordinate of infospot
+const infospotZ = -1003.95; // Z-coordinate of infospot
 
-// Event listener for mouse click
-viewer.container.addEventListener("click", (event) => {
-  const screenX = event.clientX;
-  const screenY = event.clientY;
+// Create an infospot at the specified position
+const infospot = new PANOLENS.Infospot(100, PANOLENS.DataImage.Info);
+infospot.position.set(infospotX, infospotY, infospotZ);
+viewer.add(infospot);
 
-  const canvasWidth = viewer.renderer.domElement.width;
-  const canvasHeight = viewer.renderer.domElement.height;
-
-  const vector = new THREE.Vector2(
-    (screenX / canvasWidth) * 2 - 1,
-    -(screenY / canvasHeight) * 2 + 1
-  );
-
-  const raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera(vector, viewer.camera);
-
-  const intersects = raycaster.intersectObjects(viewer.scene.children, true);
-  if (intersects.length > 0) {
-    const position = intersects[0].point;
-    positionDisplay.textContent = `Position: x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(2)}, z: ${position.z.toFixed(2)}`;
-  }
+// Event listener for infospot click
+infospot.addEventListener("click", () => {
+  window.location.href = "https://youtube.com"; // Replace with your desired URL
 });

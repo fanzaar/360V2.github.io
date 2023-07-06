e09effef78a076c506c47a53f492a642569f791b
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-07-06 21:38:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,23 @@ document.body.appendChild(positionDisplay);
 
 // Event listener for mouse click
 viewer.container.addEventListener("click", (event) => {
-  const position = viewer.getControlPosition(event);
-  positionDisplay.textContent = `Position: x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(2)}, z: ${position.z.toFixed(2)}`;
+  const screenX = event.clientX;
+  const screenY = event.clientY;
+
+  const canvasWidth = viewer.renderer.domElement.width;
+  const canvasHeight = viewer.renderer.domElement.height;
+
+  const vector = new THREE.Vector2(
+    (screenX / canvasWidth) * 2 - 1,
+    -(screenY / canvasHeight) * 2 + 1
+  );
+
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera(vector, viewer.camera);
+
+  const intersects = raycaster.intersectObjects(viewer.scene.children, true);
+  if (intersects.length > 0) {
+    const position = intersects[0].point;
+    positionDisplay.textContent = `Position: x: ${position.x.toFixed(2)}, y: ${position.y.toFixed(2)}, z: ${position.z.toFixed(2)}`;
+  }
 });

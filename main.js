8619c8e8977592744c90cdba03970663f7a23942
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-07-09 04:55:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
-// Create the panorama image
-const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
-
-// Get the container element
+const panoramaImage = new PANOLENS.ImagePanorama("images/image2.jpg");
 const imageContainer = document.querySelector(".image-container");
 
-// Create the viewer with the container
-const viewer = new PANOLENS.Viewer({ container: imageContainer });
-
-// Add the panorama image to the viewer
-viewer.add(panoramaImage);
-viewer.addEventListener("panorama-load", function() {
-  const cameraPosition = viewer.getControl().getPosition();
-  console.log("Camera Position:", cameraPosition);
+const viewer = new PANOLENS.Viewer({
+  container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
+  controlBar: false,
 });
 
-
-// Create an infospot at the specified position
-const infospot = new PANOLENS.Infospot(200, PANOLENS.DataImage.Info);
-infospot.position.set(0, 0,0); // Replace with the desired coordinates
-viewer.add(infospot);
-
+viewer.add(panoramaImage);

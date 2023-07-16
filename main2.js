3d4af05e7689bf00d4ca551c9f144cc40a65c52b
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-07-16 12:26:16 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -11,9 +11,7 @@ const viewer = new PANOLENS.Viewer({
 
 viewer.add(panoramaImage);
 var infospot;
-infospot = new PANOLENS.Infospot(300);
+infospot = new PANOLENS.Infospot();
 infospot.position.set( -1748.97, -3031.60, 3560.66 );
 infospot.addHoverText( 'This way to kitchen' );
-infospot.addEventListener("click", () => {
-  window.location.href = "https://youtube.com"; // Replace with the URL of the desired page
-});
+

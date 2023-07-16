5be06beda4d61b4a7226d805529d0f02fc759051
fanzaar <125753578+fanzaar@users.noreply.github.com>
2023-07-16 12:57:45 +0530

    Update main3.js

--- main3.js
+++ main3.js
@@ -10,3 +10,16 @@ const viewer = new PANOLENS.Viewer({
 });
 
 viewer.add(panoramaImage);
+//-3055.79, -2654.36, 2925.52
+var clickPosition = new THREE.Vector3(-3055.79, -2654.36, 2925.52);
+viewer.addEventListener('click', function(event) {
+  // Check if the click event occurred within a certain range of the specified position
+  var distanceThreshold = 200;
+  var distance = viewer.camera.position.distanceTo(clickPosition);
+
+  if (distance < distanceThreshold) {
+    // The click event occurred within the range of the specified position
+    // Perform your desired actions here
+    console.log('Clicked on position:', clickPosition);
+  }
+});

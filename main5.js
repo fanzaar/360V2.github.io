0285486941ffffa333d53e3ce8fa8757bc091ec4
fanzaar <fanzar.25@gmail.com>
2023-07-26 20:34:04 +0530

    update 2.1

---
+++ main5.js
@@ -0,0 +1,22 @@
+const panoramaImage = new PANOLENS.ImagePanorama("images/room2.jpg");
+const imageContainer = document.querySelector(".image-container");
+
+const viewer = new PANOLENS.Viewer({
+  container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
+  controlBar: false,
+  output:'console',
+});
+var infospot;
+infospot = new PANOLENS.Infospot(700,"images/arrow1.png");
+infospot.position.set( -3681.86, -2855.32, 1840.94 );
+infospot.addHoverText( 'This way to Hall' );
+infospot.addEventListener("click", () => {
+  // Code to be executed when infospot is clicked
+  // Replace this with your own code
+  window.location.href = "mains2.html"; // Redirect to another page
+});
+panoramaImage.add(infospot);
+viewer.add(panoramaImage);
+//-3055.79, -2654.36, 2925.52
